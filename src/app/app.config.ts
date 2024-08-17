17edb060ae7d5fe808f5ba79849cf177e86f3214
sanjay <sanjayhaleri@gmail.com>
2024-08-17 18:00:59 +0530

    added essential libs files and settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHighlightOptions } from 'ngx-highlightjs';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js')
+    })
+  ]
 };
